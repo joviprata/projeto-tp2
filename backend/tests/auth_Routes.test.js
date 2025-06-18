61a7bd9855cea6755b41418b3ceac171c3856a76
lopogamer <232036958@aluno.unb.br>
2025-06-18 10:30:12 -0300

    TDD RED : Tem que Retorna 400 quando tiver algum campo vazio com a mensagem de erro específica

--- backend/tests/auth_Routes.test.js
+++ backend/tests/auth_Routes.test.js
@@ -8,4 +8,48 @@ describe("Register Gerente Route", () => {
     expect(response.body).toHaveProperty("error");
     expect(response.body.error).toBe("Dados incompletos");
   });
+
+  it("Tem que Retorna 400 quando tiver algum campo vazio com a mensagem de erro específica", async () => {
+    const ResponseSemEmail = await request(app).post("/registerGerente").send({
+      name: "João",
+      email: "",
+      password: "senha123",
+      address: "Rua A, 123",
+    });
+    expect(ResponseSemEmail.status).toBe(400);
+    expect(ResponseSemEmail.body).toHaveProperty("error");
+    expect(ResponseSemEmail.body.error).toBe("Email é obrigatório");
+
+    const ResponseSemSenha = await request(app).post("/registerGerente").send({
+      name: "João",
+      email: "joao@example.com",
+      password: "",
+      address: "Rua A, 123",
+    });
+    expect(ResponseSemSenha.status).toBe(400);
+    expect(ResponseSemSenha.body).toHaveProperty("error");
+    expect(ResponseSemSenha.body.error).toBe("Senha é obrigatória");
+
+    const ResponseSemEndereco = await request(app)
+      .post("/registerGerente")
+      .send({
+        name: "João",
+        email: "joao@example.com",
+        password: "senha123",
+        address: "Rua A, 123",
+      });
+    expect(ResponseSemEndereco.status).toBe(400);
+    expect(ResponseSemEndereco.body).toHaveProperty("error");
+    expect(ResponseSemEndereco.body.error).toBe("Endereço é obrigatório");
+
+    const ResponseSemNome = await request(app).post("/registerGerente").send({
+      name: "",
+      email: "joao@example.com",
+      password: "senha123",
+      address: "Rua A, 123",
+    });
+    expect(ResponseSemNome.status).toBe(400);
+    expect(ResponseSemNome.body).toHaveProperty("error");
+    expect(ResponseSemNome.body.error).toBe("Nome é obrigatório");
+  });
 });
