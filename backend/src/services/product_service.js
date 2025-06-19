9fd95afe177ce267807545564022498dd36f71db
JoaoG0988 <gabrielmachado.6677@gmail.com>
2025-06-19 15:31:57 -0300

    TDD GREEN : Deve retornar 400 quando tiver algum campo vazio com a mensagem de erro específica

--- backend/src/services/product_service.js
+++ backend/src/services/product_service.js
@@ -4,6 +4,24 @@ const registerProduct = async (body) => {
     if (!body || Object.keys(body).length === 0) {
         return { status : 400, error: "Dados imcompletos ou inválidos" };
     }
+
+    const { name, CodigoDeBarras, Data, Descricao } = body;
+
+    if (!name || name.trim() === "") {
+        return { status: 400, error: "O campo nome é obrigatório" };
+    }
+
+    if (!CodigoDeBarras || CodigoDeBarras.trim() === "") {
+        return { status: 400, error: "O campo Código de Barras é obrigatório" };
+    }
+
+    if (!Data || Data.trim() === "") {
+        return { status: 400, error: "O campo Data é obrigatório" };
+    }
+
+    if (!Descricao || Descricao.trim() === "") {
+        return { status: 400, error: "O campo Descrição é obrigatório" };
+    }
 };
 
 module.exports = {
