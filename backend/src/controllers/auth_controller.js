15e40d7269146f6ac6821a5717c0c06bf45ca2d1
lopogamer <232036958@aluno.unb.br>
2025-06-20 12:16:30 -0300

    test(tdd/refactor) Move a logica de verificaçao do body da requisaçao do service para o controller e retorna o status em int ao inves de string

--- backend/src/controllers/auth_controller.js
+++ backend/src/controllers/auth_controller.js
@@ -1,44 +1,56 @@
 const authService = require("../services/auth_service");
 
 const login = async (req, res) => {
+  if (Object.keys(req.body).length === 0 || Object.keys(req.body).length > 2) {
+    return res.status(400).json({ error: "Requisição inválida" });
+  }
+  if (!req.body || !req.body.email || !req.body.password) {
+    return res.status(400).json({ error: "Requisição inválida" });
+  }
   try {
     const loginData = req.body;
     const results = await authService.login(loginData);
-    if (results.status === "401") {
+    if (results.status === 401) {
       return res.status(401).json({ error: results.error });
     }
-    if (results.status === "400") {
-      return res.status(400).json({ error: results.error });
-    }
-    if (results.status === "200") {
+    if (results.status === 200) {
       return res.status(200).json({
         message: results.message,
         userId: results.userId,
         role: results.role,
       });
     }
-    res.status(501);
+    res.status(500).json({ error: "Internal Server Error" });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
 const registerGerente = async (req, res) => {
+  if (Object.keys(req.body).length === 0 || Object.keys(req.body).length > 4) {
+    return res.status(400).json({ error: "Requisição inválida" });
+  }
+  if (
+    !req.body ||
+    !req.body.name ||
+    !req.body.email ||
+    !req.body.password ||
+    !req.body.address
+  ) {
+    return res.status(400).json({ error: "Requisição inválida" });
+  }
   try {
     const gerenteData = req.body;
     const results = await authService.registerGerente(gerenteData);
-    if (results.status === "400") {
-      return res.status(400).json({ error: results.error });
-    }
-    if (results.status === "201") {
+    if (results.status === 201) {
       return res.status(201).json({
         message: results.message,
         supermarketId: results.supermarketId,
       });
     }
-    if (results.status === "409") {
+    if (results.status === 409) {
       return res.status(409).json({ error: results.error });
     }
-    res.status(results.status);
+    res.status(500).json({ error: "Internal Server Error" });
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
--- backend/src/services/auth_service.js
+++ backend/src/services/auth_service.js
@@ -1,77 +1,69 @@
 const prismaDatabase = require("../prismaClient");
 
-const login = async (loginData) => {
-  if (
-    Object.keys(loginData).length === 0 ||
-    Object.keys(loginData).length > 2
-  ) {
-    return { status: "400", error: "Requisição inválida" };
-  }
-  if (!loginData || !loginData.email || !loginData.password) {
-    return { status: "400", error: "Requisição inválida" };
-  }
+const isPasswordValid = (inputPassword, storedPassword) =>
+  inputPassword === storedPassword;
+
+const login = async ({ email, password }) => {
   try {
-    const { email, password } = loginData;
     const user = await prismaDatabase.user.findUnique({
-      where: {
-        email: email,
-      },
+      where: { email },
     });
+
     if (!user) {
-      return { status: "401", error: "Email inválido" };
+      return {
+        status: 401,
+        error: "Email inválido",
+      };
     }
-    if (user.password !== password) {
-      return { status: "401", error: "Senha inválida" };
+
+    if (!isPasswordValid(password, user.password)) {
+      return {
+        status: 401,
+        error: "Senha inválida",
+      };
     }
+
     return {
-      status: "200",
+      status: 200,
       message: "Login realizado com sucesso",
       userId: user.id,
       role: user.role,
     };
   } catch (error) {
     console.error("Erro ao autenticar usuário:", error);
-    return { status: "500", error: "Erro interno do servidor" };
+    return {
+      status: 500,
+      error: "Erro interno do servidor",
+    };
   }
 };
-const registerGerente = async (gerenteData) => {
-  if (
-    Object.keys(gerenteData).length === 0 ||
-    Object.keys(gerenteData).length > 4
-  ) {
-    return { status: "400", error: "Requisição inválida" };
-  }
-  if (
-    !gerenteData ||
-    !gerenteData.name ||
-    !gerenteData.email ||
-    !gerenteData.password ||
-    !gerenteData.address
-  ) {
-    return { status: "400", error: "Requisição inválida" };
-  }
+
+const registerGerente = async ({ name, email, password, address }) => {
   try {
-    existingGerente = await prismaDatabase.user.findUnique({
-      where: {
-        email: gerenteData.email,
-      },
+    const existingUser = await prismaDatabase.user.findUnique({
+      where: { email },
     });
-    if (existingGerente) {
-      return { status: "409", error: "Email já em uso" };
+
+    if (existingUser) {
+      return {
+        status: 409,
+        error: "Email já em uso",
+      };
     }
-    newGerente = await prismaDatabase.user.create({
+
+    const newGerente = await prismaDatabase.user.create({
       data: {
-        name: gerenteData.name,
-        email: gerenteData.email,
-        password: gerenteData.password,
+        name,
+        email,
+        password,
         role: "GERENTE",
       },
     });
 
     const newSupermarket = await prismaDatabase.supermarket.create({
       data: {
-        name: gerenteData.name,
-        address: gerenteData.address,
+        name,
+        address,
         manager: {
           connect: {
             id: newGerente.id,
@@ -79,16 +71,19 @@ const registerGerente = async (gerenteData) => {
         },
       },
     });
+
     return {
-      status: "201",
+      status: 201,
       message: "Gerente registrado com sucesso",
       supermarketId: newSupermarket.id,
     };
   } catch (error) {
     console.error("Erro ao registrar gerente:", error);
-    return { status: "500", error: "Erro interno do servidor" };
+    return {
+      status: 500,
+      error: "Erro interno do servidor",
+    };
   }
-  return { status: "500" };
 };
 
 module.exports = {
