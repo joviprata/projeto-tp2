6f146ad477fbafb54c32765b428e12cd8ef0478e
RafaBonach <rafa.bonach@gmail.com>
2025-06-21 17:21:37 -0300

    refactor: Ajustanto arquivo auth_routes.test.js

--- backend/tests/auth_routes.test.js
+++ backend/tests/auth_routes.test.js
@@ -1,316 +1,55 @@
 const request = require("supertest");
 const app = require("../src/server");
-const prismaDatabase = require("../src/prismaClient");
-beforeAll(async () => {
-  const tableNames = [
-    "users",
-    "supermercado",
-    "produtos",
-    "registros_de_preco",
-    "listas_de_compra",
-    "itens_da_lista",
-  ];
-  console.log("Limpando tabelas antes dos testes...");
-  for (const tableName of tableNames) {
-    await prismaDatabase.$executeRawUnsafe(
-      `TRUNCATE TABLE "${tableName}" RESTART IDENTITY CASCADE;`
-    );
-  }
-});
-afterAll(async () => {
-  await prismaDatabase.$disconnect();
-});
-describe("POST /auth/register/manager - Validação de campos obrigatórios", () => {
-  it.each([
-    {
-      case: "corpo vazio",
-      payload: {},
-    },
-    {
-      case: "faltando nome, senha e endereço",
-      payload: { email: "test@example.com" },
-    },
-    {
-      case: "faltando email, senha e endereço",
-      payload: { name: "Supermercado Teste" },
-    },
-    {
-      case: "faltando a senha",
-      payload: {
-        name: "Supermercado Teste",
-        email: "test@example.com",
-        address: "Rua Teste, 123",
-      },
-    },
-    {
-      case: "faltando o email",
-      payload: {
-        name: "Supermercado Teste",
+
+describe("Register Gerente Route", () => {
+  it("Tem que Retorna 400 quando não for passado nenhum dado com a mensagem de erro", async () => {
+    const response = await request(app).post("/registerGerente").send({});
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("error");
+    expect(response.body.error).toBe("Dados incompletos");
+  });
+
+  it("Tem que Retorna 400 quando tiver algum campo vazio com a mensagem de erro específica", async () => {
+    const ResponseSemEmail = await request(app).post("/registerGerente").send({
+      name: "João",
+      email: "",
+      password: "senha123",
+      address: "Rua A, 123",
+    });
+    expect(ResponseSemEmail.status).toBe(400);
+    expect(ResponseSemEmail.body).toHaveProperty("error");
+    expect(ResponseSemEmail.body.error).toBe("Email é obrigatório");
+
+    const ResponseSemSenha = await request(app).post("/registerGerente").send({
+      name: "João",
+      email: "joao@example.com",
+      password: "",
+      address: "Rua A, 123",
+    });
+    expect(ResponseSemSenha.status).toBe(400);
+    expect(ResponseSemSenha.body).toHaveProperty("error");
+    expect(ResponseSemSenha.body.error).toBe("Senha é obrigatória");
+
+    const ResponseSemEndereco = await request(app)
+      .post("/registerGerente")
+      .send({
+        name: "João",
+        email: "joao@example.com",
         password: "senha123",
-        address: "Rua Teste, 123",
-      },
-    },
-    {
-      case: "faltando o endereço",
-      payload: {
-        name: "Supermercado Teste",
-        password: "senha123",
-        email: "test@example.com",
-      },
-    },
-    {
-      case: "nome em branco",
-      payload: {
-        name: "",
-        email: "",
-        password: "senha123",
-        address: "Rua Teste, 123",
-      },
-    },
-    {
-      case: "senha em branco",
-      payload: {
-        name: "Supermercado Teste",
-        email: "test@example.com",
-        password: "",
-        address: "Rua Teste, 123",
-      },
-    },
-    {
-      case: "email em branco",
-      payload: {
-        name: "Supermercado Teste",
-        password: "senha123",
-        email: "",
-        address: "Rua Teste, 123",
-      },
-    },
-    {
-      case: "endereço em branco",
-      payload: {
-        name: "Supermercado Teste",
-        password: "senha123",
-        email: "test@example.com",
         address: "",
-      },
-    },
-    {
-      case: "Com mais de 4 campos na requisição",
-      payload: {
-        name: "Supermercado Teste",
-        email: "test@example.com",
-        password: "senha123",
-        address: "Rua Teste, 123",
-        extraField: "extraValue",
-      },
-    },
-  ])("Deve retornar 400 quando $case", async ({ payload }) => {
-    const response = await request(app)
-      .post("/auth/register/manager")
-      .send(payload);
+      });
+    expect(ResponseSemEndereco.status).toBe(400);
+    expect(ResponseSemEndereco.body).toHaveProperty("error");
+    expect(ResponseSemEndereco.body.error).toBe("Endereço é obrigatório");
 
-    expect(response.status).toBe(400);
-    expect(response.headers["content-type"]).toMatch(/json/);
-    expect(response.body).toHaveProperty("error");
-    expect(response.body.error).toBe("Requisição inválida");
-  });
-});
-describe("POST /auth/register/manager - Registro de gerente com sucesso", () => {
-  it("Deve registrar um gerente com sucesso", async () => {
-    const payload = {
-      name: "Supermercado Teste",
-      email: "test@example.com",
+    const ResponseSemNome = await request(app).post("/registerGerente").send({
+      name: "",
+      email: "joao@example.com",
       password: "senha123",
-      address: "Rua Teste, 123",
-    };
-
-    const response = await request(app)
-      .post("/auth/register/manager")
-      .send(payload);
-
-    expect(response.status).toBe(201);
-    expect(response.headers["content-type"]).toMatch(/json/);
-    expect(response.body).toHaveProperty("message");
-    expect(response.body.message).toBe("Gerente registrado com sucesso");
-    expect(response.body).toHaveProperty("supermarketId");
-
-    expect(response.body.supermarketId).toEqual(expect.any(Number));
-    expect(response.body.supermarketId).toBeGreaterThan(0);
-    const supermarket = await prismaDatabase.supermarket.findUnique({
-      where: { id: parseInt(response.body.supermarketId) },
+      address: "Rua A, 123",
     });
-    expect(supermarket).not.toBeNull();
-    expect(supermarket.name).toBe(payload.name);
-    expect(supermarket.address).toBe(payload.address);
-    const gerente = await prismaDatabase.user.findUnique({
-      where: { email: payload.email },
-    });
-    expect(gerente).not.toBeNull();
-    expect(gerente.name).toBe(payload.name);
-    expect(gerente.email).toBe(payload.email);
-    expect(gerente.role).toBe("GERENTE");
-  });
-});
-describe("POST /auth/register/manager - validaçao de criaçao duplicada", () => {
-  it("Deve retornar 409 quando o email já estiver em uso", async () => {
-    const payload = {
-      name: "Supermercado Teste",
-      email: "test@example.com",
-      password: "senha123",
-      address: "Rua Teste, 123",
-    };
-
-    await request(app).post("/auth/register/manager").send(payload);
-
-    const response = await request(app)
-      .post("/auth/register/manager")
-      .send(payload);
-
-    expect(response.status).toBe(409);
-    expect(response.headers["content-type"]).toMatch(/json/);
-    expect(response.body).toHaveProperty("error");
-    expect(response.body.error).toBe("Email já em uso");
-  });
-});
-
-describe("POST /auth/login - Login do gerente com erro de autenticação", () => {
-  it("Deve retornar 401 quando o email estiver incorreto", async () => {
-    const Registerpayload = {
-      name: "Supermercado Teste",
-      email: "email@teste.com",
-      password: "senha123",
-      address: "Rua Teste, 123",
-    };
-    await request(app).post("/auth/register/manager").send(Registerpayload);
-    const Loginpayload = {
-      email: "email@incorreto.com",
-      password: "senha123",
-    };
-    const response = await request(app).post("/auth/login").send(Loginpayload);
-    expect(response.status).toBe(401);
-    expect(response.headers["content-type"]).toMatch(/json/);
-    expect(response.body).toHaveProperty("error");
-    expect(response.body.error).toBe("Email inválido");
-  });
-  it("Deve retornar 401 quando a senha estiver incorreta", async () => {
-    const Registerpayload = {
-      name: "Supermercado Teste",
-      email: "email@teste.com",
-      password: "senha123",
-      address: "Rua Teste, 123",
-    };
-    await request(app).post("/auth/register/manager").send(Registerpayload);
-    const Loginpayload = {
-      email: "email@incorreto.com",
-      password: "senha123",
-    };
-    const response = await request(app).post("/auth/login").send(Loginpayload);
-    expect(response.status).toBe(401);
-    expect(response.headers["content-type"]).toMatch(/json/);
-    expect(response.body).toHaveProperty("error");
-    expect(response.body.error).toBe("Email inválido");
-  });
-  it("Deve retornar 401 quando a senha estiver incorreta", async () => {
-    const Registerpayload = {
-      name: "Supermercado Teste",
-      email: "email@teste.com",
-      password: "senha123",
-      address: "Rua Teste, 123",
-    };
-    await request(app).post("/auth/register/manager").send(Registerpayload);
-    const Loginpayload = {
-      email: "email@teste.com",
-      password: "senha_incorreta",
-    };
-    const response = await request(app).post("/auth/login").send(Loginpayload);
-    expect(response.status).toBe(401);
-    expect(response.headers["content-type"]).toMatch(/json/);
-    expect(response.body).toHaveProperty("error");
-    expect(response.body.error).toBe("Senha inválida");
-  });
-});
-
-describe("POST /auth/login - Login do gerente com erro de requisição", () => {
-  it.each([
-    {
-      case: "corpo vazio",
-      payload: {},
-    },
-    {
-      case: "email e senha em branco",
-      payload: { email: "", password: "" },
-    },
-    {
-      case: "email em branco",
-      payload: { email: "", password: "senha123" },
-    },
-    {
-      case: "senha em branco",
-      payload: { email: "email@teste.com", password: "" },
-    },
-    {
-      case: "mais de 2 campos na requisição",
-      payload: {
-        email: "email@teste.com",
-        password: "senha123",
-        extraField: "extraValue",
-      },
-    },
-  ])("Deve retornar 400 quando o $case", async ({ payload }) => {
-    Registerpayload = {
-      name: "Supermercado Teste",
-      email: "email@teste.com",
-      password: "senha123",
-      address: "Rua Teste, 123",
-    };
-    await request(app).post("/auth/register/manager").send(Registerpayload);
-    const response = await request(app).post("/auth/login").send(payload);
-    expect(response.status).toBe(400);
-    expect(response.headers["content-type"]).toMatch(/json/);
-    expect(response.body).toHaveProperty("error");
-    expect(response.body.error).toBe("Requisição inválida");
-  });
-});
-
-describe("POST /auth/login - Login do gerente com sucesso", () => {
-  it("Deve fazer login com sucesso", async () => {
-    const Registerpayload = {
-      name: "Supermercado Teste",
-      email: "email@teste.com",
-      password: "senha123",
-      address: "Rua Teste, 123",
-    };
-    const loginPayload = {
-      email: "email@teste.com",
-      password: "senha123",
-    };
-    await request(app).post("/auth/register/manager").send(Registerpayload);
-    const response = await request(app).post("/auth/login").send(loginPayload);
-    expect(response.status).toBe(200);
-    expect(response.headers["content-type"]).toMatch(/json/);
-    expect(response.body).toHaveProperty("message");
-    expect(response.body.message).toBe("Login realizado com sucesso");
-    expect(response.body).toHaveProperty("userId");
-    expect(response.body.userId).toEqual(expect.any(Number));
-    expect(response.body.userId).toBeGreaterThan(0);
-    expect(response.body).toHaveProperty("role");
-    expect(response.body.role).toBe("GERENTE");
-    const supermarket = await prismaDatabase.supermarket.findUnique({
-      where: { managerId: response.body.userId },
-    });
-    expect(supermarket).not.toBeNull();
-    expect(supermarket.name).toBe(Registerpayload.name);
-    expect(supermarket.address).toBe(Registerpayload.address);
-
-    const gerente = await prismaDatabase.user.findUnique({
-      where: { email: Registerpayload.email },
-    });
-    expect(gerente).not.toBeNull();
-    expect(gerente.name).toBe(Registerpayload.name);
-    expect(gerente.email).toBe(Registerpayload.email);
-    expect(gerente.role).toBe("GERENTE");
-    expect(gerente.id).toBe(response.body.userId);
-    expect(response.body.userId).toBe(gerente.id);
-    expect(response.body.role).toBe(gerente.role);
+    expect(ResponseSemNome.status).toBe(400);
+    expect(ResponseSemNome.body).toHaveProperty("error");
+    expect(ResponseSemNome.body.error).toBe("Nome é obrigatório");
   });
 });
