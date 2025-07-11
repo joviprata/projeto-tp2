257593c80a3dbda7f4b9c28168ab4fdf594407d5
lopogamer <232036958@aluno.unb.br>
2025-07-11 17:19:02 -0300

    feat: implementar gerenciamento de registros de preço com operações CRUD
        Adicionado price_records_controller para lidar com requisições HTTP para registros de preço.
    
        Criada price_records_route para definir rotas relacionadas a registros de preço.
    
        Desenvolvido price_records_service para interagir com o banco de dados para operações de registro de preço.
    
        Implementados testes para as rotas de registros de preço para garantir funcionalidade e confiabilidade.

--- backend/tests/all.test.js
+++ backend/tests/all.test.js
@@ -3,3 +3,4 @@ require('./supermarket_routes.test');
 require('./Product_Routes.test');
 require('./user_routes.test');
 require('./product_lists_routes.test');
+require('./price_records_routes.test');
